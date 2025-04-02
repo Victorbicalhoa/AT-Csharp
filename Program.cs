3db694b493e8b6c6052cf229ba063fd70beb8ba1
Victor <Victorbicalhoa@outlook.com>
2025-04-01 21:27:38 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, meu nome é Victor!");
+        Console.WriteLine("Nasci em [sua data de nascimento] e estou aprendendo C#!");
+    }
+}
