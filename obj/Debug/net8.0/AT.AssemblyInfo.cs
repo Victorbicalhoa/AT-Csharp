f21ccef2299d75e7fce68414731efdc470f235e9
Victor <Victorbicalhoa@outlook.com>
2025-04-01 21:33:48 -0300

    Second commit

--- obj/Debug/net8.0/AT.AssemblyInfo.cs
+++ obj/Debug/net8.0/AT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db694b493e8b6c6052cf229ba063fd70beb8ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
